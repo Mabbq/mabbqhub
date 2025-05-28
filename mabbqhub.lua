c0200398026969240aac62e55bb4db527682c49f
Mabbq <109665213+Mabbq@users.noreply.github.com>
2025-05-28 23:30:29 +0300

    Update mabbqhub.lua

--- mabbqhub.lua
+++ mabbqhub.lua
@@ -1,6 +1,6 @@
 local args = {
     [1] = "Dash",
-    [2] = "A\255"
+    [2] = "Q\255"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("CCGLabs"):WaitForChild("RandomizeColor"):FireServer(unpack(args))
